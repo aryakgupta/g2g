b65f875f926b5608722085ddab57ab4b7853cc33
Grocermax <vinay.singh@grocermax.com>
2017-03-28 14:58:25 +0530

    change

--- wp-content/themes/nexus/header.php
+++ wp-content/themes/nexus/header.php
@@ -40,7 +40,7 @@
 							<span class="icon-bar"></span>
 							<span class="icon-bar"></span>
 						</button>
-						<a class="navbar-brand" href="#"><img src="<?php echo get_template_directory_uri();?>/photos/logo3.png" alt="" class="img-responsive"></a>
+						<a class="navbar-brand" href="#"><img src="<?php echo get_template_directory_uri();?>/images/logo3.png" alt="" class="img-responsive"></a>
 					</div>
 					
 					<!-- Collect the nav links, forms, and other content for toggling -->
